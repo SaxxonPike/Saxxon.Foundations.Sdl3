8d8a28983fc89c0e7565d757303d47b513b9b6ee
Anthony Konzel <saxxonpike@gmail.com>
2025-09-22 18:23:36 -0500

    Fix the documentation build issue

--- src/Saxxon.Foundations.Sdl3/Models/SDL_SpinLock.cs
+++ src/Saxxon.Foundations.Sdl3/Models/SDL_SpinLock.cs
@@ -1,7 +1,7 @@
 namespace Saxxon.Foundations.Sdl3.Models;
 
 /// <summary>
-/// Represents a SpinLock ID for use with SDL <see cref="SpinLock"/> functions.
+/// Represents a SpinLock ID for use with SDL SpinLock functions.
 /// </summary>
 // ReSharper disable once InconsistentNaming
 public enum SDL_SpinLock;
