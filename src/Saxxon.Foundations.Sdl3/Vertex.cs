6601b77365bb04eb0b86b508b4d9c3defef003c2
Saxxon Fox <saxxonpike@gmail.com>
2025-11-06 10:33:08 -0600

    Add extension block for SDL_Vertex*

--- src/Saxxon.Foundations.Sdl3/Vertex.cs
+++ src/Saxxon.Foundations.Sdl3/Vertex.cs
@@ -10,38 +10,26 @@ namespace Saxxon.Foundations.Sdl3;
 public static class Vertex
 {
     /// <summary>
-    /// Retrieves the color field of an SDL_Vertex.
+    /// Extensions for <see cref="SDL_Vertex"/> references.
     /// </summary>
-    public static SDL_FColor GetColor(this IntPtr<SDL_Vertex> vertex) =>
-        vertex.AsReadOnlyRef().color;
+    extension(IntPtr<SDL_Vertex> vertex)
+    {
+        /// <summary>
+        /// Color of an <see cref="SDL_Vertex"/>.
+        /// </summary>
+        public ref SDL_FColor Color =>
+            ref vertex.AsRef().color;
 
-    /// <summary>
-    /// Retrieves the position of an SDL_Vertex.
-    /// </summary>
-    public static SDL_FPoint GetPosition(this IntPtr<SDL_Vertex> vertex) =>
-        vertex.AsReadOnlyRef().position;
+        /// <summary>
+        /// Position of an <see cref="SDL_Vertex"/>.
+        /// </summary>
+        public ref SDL_FPoint Position =>
+            ref vertex.AsRef().position;
 
-    /// <summary>
-    /// Retrieves the texture coordinate of an SDL_Vertex.
-    /// </summary>
-    public static SDL_FPoint GetTexCoord(this IntPtr<SDL_Vertex> vertex) =>
-        vertex.AsReadOnlyRef().tex_coord;
-
-    /// <summary>
-    /// Sets the color of an SDL_Vertex.
-    /// </summary>
-    public static void SetColor(this IntPtr<SDL_Vertex> vertex, SDL_FColor color) =>
-        vertex.AsRef().color = color;
-
-    /// <summary>
-    /// Sets the position of an SDL_Vertex.
-    /// </summary>
-    public static void SetPosition(this IntPtr<SDL_Vertex> vertex, SDL_FPoint position) =>
-        vertex.AsRef().position = position;
-
-    /// <summary>
-    /// Sets the texture coordinate of an SDL_Vertex.
-    /// </summary>
-    public static void SetTexCoord(this IntPtr<SDL_Vertex> vertex, SDL_FPoint texCoord) =>
-        vertex.AsRef().tex_coord = texCoord;
+        /// <summary>
+        /// Texture coordinate of an <see cref="SDL_Vertex"/>.
+        /// </summary>
+        public ref SDL_FPoint TexCoord =>
+            ref vertex.AsRef().tex_coord;
+    }
 }
