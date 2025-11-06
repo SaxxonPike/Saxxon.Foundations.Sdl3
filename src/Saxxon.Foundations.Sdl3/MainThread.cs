d1e6012fba920f0cdd4f9bd69cf1bb5e04407e08
Saxxon Fox <saxxonpike@gmail.com>
2025-11-06 09:50:46 -0600

    Add documentation

--- src/Saxxon.Foundations.Sdl3/MainThread.cs
+++ src/Saxxon.Foundations.Sdl3/MainThread.cs
@@ -12,6 +12,15 @@ namespace Saxxon.Foundations.Sdl3;
 [PublicAPI]
 public static class MainThread
 {
+    /// <summary>
+    /// Calls a function on the main thread during event processing.
+    /// </summary>
+    /// <param name="func">
+    /// The callback to call on the main thread.
+    /// </param>
+    /// <param name="waitComplete">
+    /// True to wait for the callback to complete, false to return immediately.
+    /// </param>
     public static unsafe void Run(Action func, bool waitComplete)
     {
         var userData = UserDataStore.Add(func);
@@ -29,6 +38,18 @@ public static class MainThread
         }
     }
 
+    /// <summary>
+    /// Determines whether the currently executing thread is the main thread.
+    /// </summary>
+    /// <returns>
+    /// True if this thread is the main thread, or false otherwise.
+    /// </returns>
+    /// <remarks>
+    /// On Apple platforms, the main thread is the thread that runs your program's main() entry point.
+    /// On other platforms, the main thread is the one that calls SDL_Init(SDL_INIT_VIDEO), which should usually
+    /// be the one that runs your program's main() entry point. If you are using the main callbacks, SDL_AppInit(),
+    /// SDL_AppIterate(), and SDL_AppQuit() are all called on the main thread.
+    /// </remarks>
     public static bool IsCurrent()
     {
         return SDL_IsMainThread();
